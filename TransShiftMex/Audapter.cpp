eb7b2be50787a82aadce0d73bd760d63e31ff9a9
Chris Naber <cwnaber@wisc.edu>
2021-04-26 15:36:44 -0500

    upped clamp length from 257 -> 2048

--- TransShiftMex/Audapter.cpp
+++ TransShiftMex/Audapter.cpp
@@ -427,7 +427,7 @@ Audapter::Audapter() :
 
 	/* CWN 2020 formant clamping*/	// taimComp
 	p.bClampFormants = 0;	// use clamped formants piped in from MATLAB as opposed to Audapter-calculated formants
-	for (n = 0; n < pfNPoints; n++) {
+	for (n = 0; n < maxNClampFrames; n++) {
 		p.clamp_f1[n] = 0;
 		p.clamp_f2[n] = 0;
 	}
@@ -1141,22 +1141,22 @@ void *Audapter::setGetParam(bool bSet,
 	else if (ns == string("clampf1")) {		// taimComp
 		ptr = (void*)p.clamp_f1;
 
-		if (bSet && (nPars > pfNPoints))
+		if (bSet && (nPars > maxNClampFrames))
 			mexErrMsgTxt("Clamped F1 value array is too long");
 		
-			// this reads: "if nPars is less than pfNPoints, len = nPars. Else, len = pfNPoints".
-		//len = (nPars < pfNPoints) ? nPars : pfNPoints;	// TODO this should work, but nPars is 0 on subsequent loops (including each trial) after initializing
-		len = pfNPoints;
+			// this reads: "if nPars is less than maxNClampFrames, len = nPars. Else, len = maxNClampFrames".
+		//len = (nPars < maxNClampFrames) ? nPars : maxNClampFrames;	// TODO this should work, but nPars is 0 on subsequent loops (including each trial) after initializing
+		len = maxNClampFrames;
 
 	}
 	else if (ns == string("clampf2")) {		// taimComp
 		ptr = (void*)p.clamp_f2;
 
-		if (bSet && (nPars > pfNPoints))
+		if (bSet && (nPars > maxNClampFrames))
 			mexErrMsgTxt("Clamped F2 value array is too long");
 
-		//len = (nPars < pfNPoints) ? nPars : pfNPoints;	// TODO this should work, but nPars is 0 on subsequent loops (including each trial) after initializing
-		len = pfNPoints;
+		//len = (nPars < maxNClampFrames) ? nPars : maxNClampFrames;	// TODO this should work, but nPars is 0 on subsequent loops (including each trial) after initializing
+		len = maxNClampFrames;
 	}
 	else if (ns == string("clamposts")) {	// taimComp
 		ptr = (void*)p.clamp_osts;
@@ -1206,7 +1206,7 @@ void *Audapter::setGetParam(bool bSet,
 			}
 			// This doesn't work for some reason if clamp_f1/2 aren't pfNPoints in length
 			//else if (ns == string("clampf1") || ns == string("clampf2")) { /* Zero out the remaining part */	// taimComp
-			//	for (int i = len; i < pfNPoints; i++)
+			//	for (int i = len; i < maxNClampFrames; i++)
 			//		*((dtype*)ptr + i) = 0.0;
 			//}
 		}
@@ -1799,12 +1799,12 @@ int Audapter::handleBuffer(dtype *inFrame_ptr, dtype *outFrame_ptr, int frame_si
 			if (p.bClampFormants && stat >= p.clamp_osts[0] && stat < p.clamp_osts[1] && p.clamp_f1[clampIx] != 0) {		// taimComp
 				newPhis[0] = p.clamp_f1[clampIx] * 2 * M_PI / p.sr;	// This gets converted back to Hz when sFmts is set
 				newPhis[1] = p.clamp_f2[clampIx] * 2 * M_PI / p.sr;
-				during_trans = true;	// TODO(CWN) this is pretty hacky, should probably just have some way to circumvent check in L1852
-				if ((clampIx < pfNPoints-2) && (p.clamp_f1[clampIx+1] != 0)) {
+				during_trans = true;	// TODO(CWN) this is relatively hacky. Should probably just have some way to circumvent during_trans check in L1852
+				if ((clampIx < maxNClampFrames-2) && (p.clamp_f1[clampIx+1] != 0)) {
 					clampIx += 1;
 				}
 				else {
-					// don't increment
+					// don't increment clampIx. Thus, the final frame will be repeated indefinitely 'til clamp_osts[1] is reached
 				}
 					
 			}
